400d0927e2025fd38305c5a1cd6aef0884050f83
RyBuck44 <bucklerryan2@gmail.com>
2023-05-01 15:09:48 -0400

    started constructing player class

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pygame
 
 
+class PlayerNinja(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(self)
+        self.x = 50
+        self.y = 50
+
+        self.image = pygame.Surface([50, 50])
+        self.image.fill('green')
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+
+
 def game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 720))
@@ -9,8 +22,13 @@ def game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
         screen.fill('purple')
+
+        ###
+
         pygame.display.flip()
+
 
 
 game()
